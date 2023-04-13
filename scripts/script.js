97c9fc58bb2c1060950becd69d5269b2cd531ce2
GabrielBragaa <gbragamoura@hotmail.com>
2023-04-13 14:14:26 -0300

    implementada a entrada do usuario no chat

--- scripts/script.js
+++ scripts/script.js
@@ -5,39 +5,35 @@ function msgPortaria() {
     mensagens.innerHTML = `<div class="msg-portaria"><p><span class="horario">(09:21:45)</span>  <span class="nome">Gabriel</span>  entra na sala...</p></div>`
 }
 
-function recebeNome(nome) {
-    const status = nome.status;
-    console.log(status);
-    if (status !== 200) {
-        voltaTelaEntrada();
-    }
+function mostraNomes(lista) {
+    let listaNomes = lista.data;
+    console.log(listaNomes);
 }
 
-function mandaNome() {
-    const nomeDigitado = document.querySelector('.input-entrada');
-    const novoNome = {name: nomeDigitado.value};
-    const usuarios = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', novoNome);
-    usuarios.then(recebeNome);
-    console.log(usuarios);
-}
-
-function voltaTelaEntrada() {
-    const layoutEntrada = document.querySelector('.tela-entrada');
-    layoutEntrada.classList.remove('escondido');
-    const layoutPrincipal = document.querySelector('.tela-principal');
-    layoutPrincipal.classList.add('escondido');
-    const body = document.querySelector('body');
-    body.classList.remove('cinza');
-    mandaNome();
-}
-
-function someTelaEntrada() {
+function someTelaEntrada() {    
     const layoutEntrada = document.querySelector('.tela-entrada');
     layoutEntrada.classList.add('escondido');
     const layoutPrincipal = document.querySelector('.tela-principal');
     layoutPrincipal.classList.remove('escondido');
     const body = document.querySelector('body');
     body.classList.add('cinza');
-    mandaNome();
-    msgPortaria();
+}
+
+function erroNome() {
+    console.log('Erro!');
+}
+
+function nomeCadastrado() {
+    const promiseRecebe = axios.get('https://mock-api.driven.com.br/api/vm/uol/participants');
+    promiseRecebe.then(mostraNomes);
+    console.log('Cadastrado!');
+    someTelaEntrada();
+}
+
+function mandaNome() {
+    const nomeDigitado = document.querySelector('.input-entrada');
+    const novoNome = {name: nomeDigitado.value};
+    let usuarios = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', novoNome);
+    usuarios.then(nomeCadastrado);
+    usuarios.catch(erroNome);
 }
