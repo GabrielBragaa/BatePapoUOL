a0736a211d5af82684b206a2db05d39a000514c7
GabrielBragaa <gbragamoura@hotmail.com>
2023-04-13 15:08:39 -0300

    implementada a função de enviar mensagem

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,44 @@
 axios.defaults.headers.common['Authorization'] = 'sqTg9AiJoy0kppzmztZ9cCvk';
 
-function msgPortaria() {
-    const mensagens = document.querySelector('.mensagens');
-    mensagens.innerHTML = `<div class="msg-portaria"><p><span class="horario">(09:21:45)</span>  <span class="nome">Gabriel</span>  entra na sala...</p></div>`
+function renderizaMsg(array) {
+    let layout = document.querySelector('.mensagens');
+    layout.innerHTML = '';
+    let hora = array.data.time;
+    console.log(array.data[0].time);
+    layout.innerHTML = `<div class="msg-normal"><p><span class="horario">${hora}</span>  <span class="nome">${array.data.from}</span> para <span class="nome">${array.data.to}</span>:  ${array.data.text}</p></div>`
 }
 
-function mostraNomes(lista) {
-    let listaNomes = lista.data;
-    console.log(listaNomes);
+
+function buscaMensagem() {
+    const promiseRecebe = axios.get('https://mock-api.driven.com.br/api/vm/uol/messages');
+    promiseRecebe.then(renderizaMsg);
+}
+
+function respostaMsg() {
+    let msgDigitada = document.querySelector('.input-rodape');
+    msgDigitada.value = '';
+
+}
+
+function enviarMensagem() {
+    let msgDigitada = document.querySelector('.input-rodape');
+    const nomeDigitado = document.querySelector('.input-entrada');
+    let novaMsg = {from: nomeDigitado.value,
+	to: "Todos",
+	text: msgDigitada.value,
+	type: "message"}
+    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/messages', novaMsg);
+    promise.then(respostaMsg);
+}
+
+function verificaStatus() {
+    const nomeDigitado = document.querySelector('.input-entrada');
+    const nome = {name: nomeDigitado.value};
+    const promise = axios.post('https://mock-api.driven.com.br/api/vm/uol/status', nome);
+}
+
+function statusUsuario() {
+    setInterval(verificaStatus, 5000);
 }
 
 function someTelaEntrada() {    
@@ -20,14 +51,14 @@ function someTelaEntrada() {
 }
 
 function erroNome() {
-    console.log('Erro!');
+    alert('Esse usuário já existe! Digite outro nome.')
 }
 
 function nomeCadastrado() {
     const promiseRecebe = axios.get('https://mock-api.driven.com.br/api/vm/uol/participants');
-    promiseRecebe.then(mostraNomes);
     console.log('Cadastrado!');
     someTelaEntrada();
+    buscaMensagem();
 }
 
 function mandaNome() {
@@ -36,4 +67,5 @@ function mandaNome() {
     let usuarios = axios.post('https://mock-api.driven.com.br/api/vm/uol/participants', novoNome);
     usuarios.then(nomeCadastrado);
     usuarios.catch(erroNome);
+    statusUsuario();
 }
